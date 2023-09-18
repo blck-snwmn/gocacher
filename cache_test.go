7931975cdc289a27fc43f0dabb1656117ba391f9
blck-snwmn <whitesnowmancreator@gmail.com>
2023-09-18 21:48:48 +0900

    Add test

--- cache_test.go
+++ cache_test.go
@@ -167,3 +167,55 @@ func TestCacheDo_UseNoCache_differentKey(t *testing.T) {
 		t.Errorf("Do count = %v; want %d", calls, loop)
 	}
 }
+
+func TestCacheDo_UseNoCache_parallel(t *testing.T) {
+	var root sync.WaitGroup
+
+	for ii := 0; ii < 1000; ii++ {
+		root.Add(1)
+		go func() {
+			defer root.Done()
+
+			c := New()
+
+			// register cache
+			_, err := c.Do("key", func(key string) (interface{}, time.Time, error) {
+				return "bar", time.Now().Add(10 * time.Millisecond), nil
+			})
+			if err != nil {
+				t.Errorf("Do error = %v", err)
+				return
+			}
+
+			// wait for cache expired
+			time.Sleep(11 * time.Millisecond)
+
+			var sg sync.WaitGroup
+			var calls int64
+			for i := 0; i < 1000; i++ {
+				sg.Add(1)
+				go func() {
+					defer sg.Done()
+					v, err := c.Do("key", func(key string) (interface{}, time.Time, error) {
+						atomic.AddInt64(&calls, 1)
+						time.Sleep(10 * time.Millisecond) // wait. exapmle: http request to origin server
+						return "barz", time.Now().Add(time.Second), nil
+					})
+					if err != nil {
+						t.Errorf("Do error = %v", err)
+						return
+					}
+					if got, want := v.(string), "barz"; got != want {
+						t.Errorf("Do = %v; want %v", got, want)
+					}
+				}()
+			}
+			sg.Wait()
+
+			if calls != 1 {
+				t.Errorf("Do count = %v; want 1", calls)
+			}
+		}()
+	}
+	root.Wait()
+}
